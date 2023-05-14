6bed57bb1b6a93ba160ad9dff09653db438f16d0
Marcus Xavier <marcusxavierr123@gmail.com>
2023-05-14 09:31:10 -0300

    chore: comment lua lsp

--- lua/marcusxavier/plugins/lsp.lua
+++ lua/marcusxavier/plugins/lsp.lua
@@ -68,30 +68,30 @@ require'lspconfig'.astro.setup{
     on_attach=keymap_lsp
 }
 
-require'lspconfig'.sumneko_lua.setup {
-  capabilities=capabilities,
-  on_attach=keymap_lsp,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+-- require'lspconfig'.sumneko_lua.setup {
+--   capabilities=capabilities,
+--   on_attach=keymap_lsp,
+--   settings = {
+--     Lua = {
+--       runtime = {
+--         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+--         version = 'LuaJIT',
+--       },
+--       diagnostics = {
+--         -- Get the language server to recognize the `vim` global
+--         globals = {'vim'},
+--       },
+--       workspace = {
+--         -- Make the server aware of Neovim runtime files
+--         library = vim.api.nvim_get_runtime_file("", true),
+--       },
+--       -- Do not send telemetry data containing a randomized but unique identifier
+--       telemetry = {
+--         enable = false,
+--       },
+--     },
+--   },
+-- }
 
 -- Python LSP
 require'lspconfig'.pyright.setup{
