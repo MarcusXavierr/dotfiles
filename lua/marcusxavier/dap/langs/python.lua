249217e89dacaaba1cf9dd5bcda0e9ca269f6ee7
Marcus Xavier <marcusxavierr123@gmail.com>
2025-05-26 20:57:56 -0300

    feat(dap): add support for Python and C debugging

---
+++ lua/marcusxavier/dap/langs/python.lua
@@ -0,0 +1,18 @@
+local dap = require('dap')
+
+dap.adapters.php = {
+  type = "executable",
+  command = "php-debug-adapter", -- Installed with Mason 🤙😎🤙
+}
+
+dap.configurations.php = {
+  {
+    type = "php",
+    request = "launch",
+    name = "Listen for Xdebug",
+    port = 9000, -- Change this when using other port on Dockerfile
+    pathMappings = {
+      ["/app"] = "${workspaceFolder}" -- Change this when using other workdir on Dockerfile
+    }
+  }
+}
